692bf72c87d94cd4cf2e5d8be0c32f37fbc27e1d
Alexandr <theobroma222@gmail.com>
2017-01-15 18:17:07 +0200

    Uniqueness Validation

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,56 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
-import validateInput from '../shared/validations/signup';
+import _isEmpty from 'lodash/isEmpty';
+import commonValidations from '../shared/validations/signup';
 
 let router = express.Router();
 
 import User from '../models/user';
 
-router.post('/', (req,res)=> {
-    setTimeout(function() {
-        const {errors,isValid} = validateInput(req.body);
+
+function validateInput(data, otherValidations) {
+
+ let { errors } = otherValidations(data);
+
+ return User.find({username: data.username})
+ .then(user => {
+  if (user.length) {
+   if (user[0].username === data.username) {
+    errors.username = 'Sorry, username has been taken';
+   }
+  }
+  return {
+   errors,
+   isValid: _isEmpty(errors)
+  }
+ });
+
+}
+
+
+
+
+router.post('/', (req,res) => {
+    validateInput(req.body, commonValidations).then( ({ errors, isValid }) => {
         if(isValid){
             const {username,password} = req.body;
             const password_digest = bcrypt.hashSync('password', 10);
             const date = new Date();
-            User({username:username,password:password_digest,createdAt: date}).save()
+            const newUser = new User({
+               username: username,
+               password:password_digest,
+               createdAt: date
+            });
+            newUser.save()
                 .then( user => res.json({success:true}) )
                 .catch(err => res.status(500).json ({error:err}));
         } else {
             res.status(400).json(errors);
         }
-    }, 300);
+    })
 });
 
-router.get('/', (req,res)=> {
+router.get('/', (req,res) => {
     User.find().then((data) => {
         res.send(data);
     });
