d7969126a5d746becdb5d841f4d4552ba66f6a1f
Alexandr <theobroma222@gmail.com>
2017-02-16 13:31:47 +0200

    add npm clean

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+app.use(favicon(path.join(__dirname, 'public','build','favicon.png')));
 app.use(express.static(path.join(__dirname, 'public', 'build')));
 
 // All routes in the end
